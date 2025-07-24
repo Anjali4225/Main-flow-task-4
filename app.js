ac5e6754116864b3598943e5b17c6ec8853fe485
Anjali Kumari <anjali04225@gmail.com>
2025-07-24 20:24:18 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
+const path = require("path");
+const ejsMate = require("ejs-mate");
+
+const userRouter = require("./routes/user.js");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended: true})); 
+app.engine("ejs", ejsMate);
+
+
+
+
+main().then(() => {
+  console.log("connected to DB");
+})
+.catch((err) => {
+  console.log(err);
+});
+
+
+
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/AuthDb');
+}
+
+
+const sessionOption = {
+  secret: "mysupersecretcode",
+  resave: false,
+  saveUninitialized: true,
+  
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge:  7 * 24 * 60 * 60 * 1000, 
+    httpOnly: true, 
+  }
+}
+
+app.get("/", (req,res) => {
+  res.send("Hi, i am root");
+});
+
+app.use(session(sessionOption));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
+});
+
+app.use("/", userRouter);
+
+
+app.listen(3000, () => {
+  console.log("Server Listening to port 3000");
+})
